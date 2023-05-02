d24e17644431c3bfb709f68591d7ae9080654bd4
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2023-05-02 23:01:23 +0900

    2023. 05. 02
    Kotlin 반복문

---
+++ src/main/kotlin/Lec06Main.java
@@ -0,0 +1,22 @@
+public class Lec06Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i += 2) {
+            System.out.println(i);
+        }
+
+        //내려가는 경우
+        for(int i = 3; i >= 1; i--){
+            System.out.println(i);
+        }
+
+        int i = 1;
+        while (i <= 3) {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+
+}
---
+++ src/main/kotlin/Main5.kt
@@ -0,0 +1,27 @@
+
+
+fun main() {
+//    val numbers = listOf(1L, 2L, 3L)
+//    for (number in numbers) {
+//        println(number)
+//    }
+
+    // 1~3까지 출력
+//    for(i in 1..3) {
+//        println(i)
+//    }
+    // 3 -> 1까지 내려가는 출력
+//    for(i in 3 downTo 1) {
+//        println(i)
+//    }
+    // 1 ~ 5 까지 2씩 올라감
+//    for(i in 1..5 step 2) {
+//        println(i)
+//    }
+    // i는 밑에서 변경되니까 가변으로 설정
+    var i = 1
+    while (i <= 3) {
+        println(i)
+        i++
+    }
+}
