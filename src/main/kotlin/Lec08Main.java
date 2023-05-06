cc847ea62effd5d2ff89d9a34f834cdd5a267e0a
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2023-05-06 15:59:14 +0900

    2023. 05. 06
    Kotlin 함수, 가변인자

---
+++ src/main/kotlin/Lec08Main.java
@@ -0,0 +1,38 @@
+public class Lec08Main {
+
+    public static void main(String[] args) {
+
+    }
+//    public int max(int a, int b) {
+//        if (a > b) {
+//            return a;
+//        }
+//        return b;
+//    }
+
+    public void repeat(String str, int num, boolean useNewLine) {
+        for (int i = 1; i <= num; i++) {
+            if (useNewLine) {
+                System.out.println(str);
+            } else {
+                System.out.println(str);
+            }
+        }
+    }
+
+    // Java OverLoading
+    public void repeat(String str, int num) {
+        repeat(str, num, true);
+    }
+
+    public void repeat(String str) {
+        repeat(str, 3, true);
+    }
+
+    /// type... -> 가변인자
+    public static void printAll(String... strings) {
+        for (String str : strings) {
+            System.out.println(str);
+        }
+    }
+}
---
+++ src/main/kotlin/Main7.kt
@@ -0,0 +1,51 @@
+
+fun main() {
+    // named argument
+    //printNameAndGender(name="최태현", gender = "MALE")
+
+    // Java함수 가져다 쓸때 named argument 못씀
+    //Lec08Main.repeat(str = "A")
+
+    printAll("A", "B", "C")
+
+    val array = arrayOf("A", "B", "C")
+    printAll(*array)
+}
+
+//fun max(a: Int, b: Int): Int {
+//    return if (a > b) {
+//        a
+//    } else {
+//        b
+//    }
+//}
+
+//fun max(a: Int, b: Int) = if (a > b) a else b
+
+//반환타입은 Unit이라 생략가능
+// num 기본값은 3, useNewLine 기본값은 true라는 설정 = defalut parameter
+//fun repeat(
+//    str: String,
+//    num: Int = 3,
+//    useNewLine: Boolean = true
+//) {
+//    for(i in 1..num) {
+//        if (useNewLine) {
+//            println(str)
+//        } else {
+//            print(str)
+//        }
+//    }
+//}
+
+//fun printNameAndGender(name: String, gender: String) {
+//    println(name)
+//    println(gender)
+//}
+
+fun printAll(vararg strings: String) {
+    for (str in strings) {
+        println(str)
+    }
+}
+
