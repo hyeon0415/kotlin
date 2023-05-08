7e2e3641a67d808808f7655d63b2d34f4b1ef7e8
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2023-05-08 23:22:18 +0900

    2023. 05. 08
    Kotlin 상속

---
+++ src/main/kotlin/JavaSwimable.java
@@ -0,0 +1,7 @@
+public interface JavaSwimable {
+
+    default void act() {
+        System.out.println("어푸 어푸");
+    }
+
+}
---
+++ src/main/kotlin/Swimable.kt
@@ -0,0 +1,9 @@
+interface Swimable {
+
+    val swimAbility: Int
+    fun act() {
+        println("어푸 어푸")
+    }
+
+  // 인터페이스, 추상메소드
+}
